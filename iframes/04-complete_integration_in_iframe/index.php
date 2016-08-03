fb8509aedfa7b0bf5ad67beb33b58118747ae678
Daniel Neu <daneu+github@amazon.com>
2016-08-03 12:03:47 +0200

    Update index.php

--- iframes/04-complete_integration_in_iframe/index.php
+++ iframes/04-complete_integration_in_iframe/index.php
@@ -4,7 +4,7 @@
   </head>
 <body>
 	<h1>A complete integration running in an iframe </h1>
-    <iframe width="90%" height="800px" id="lpa_button" src="https://com-search.de/amz/samples/amazon-login-and-pay-tests/iframes/04-complete_integration_in_iframe/index.php">
+    <iframe width="90%" height="800px" id="lpa_button" src="https://com-search.de/amz/samples/amazon-login-and-pay-tests/iframes/01-standard/index.php">
     
     </iframe> 
   </body>
