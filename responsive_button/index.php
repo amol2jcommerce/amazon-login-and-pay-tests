8ae15c4f09f30bd4cd177d97bfa018668c063a33
Daniel Neu <danielneu81@gmail.com>
2016-11-09 12:57:41 +0000

    more responsive buttons

--- responsive_button/index.php
+++ responsive_button/index.php
@@ -18,11 +18,11 @@ require_once 'config.php';
       	.warning {
 			color: red;
 		}
-		
+
 		/* container emulating the button design, gradient, border, ... */
-		.PwAResponsiveContainer, .PwAResponsiveContainerNoBG {
+		.PwAResponsiveContainer, .PwAResponsiveContainerNoBG, .PwAResponsiveContainerLightGrey, .PwAResponsiveContainerDarkGrey {
 			width: 80%;
-			min-width: 260px;
+			min-width: 290px;
 			height:60px;
 			margin-left: auto;
 			margin-right:auto;
@@ -35,17 +35,28 @@ require_once 'config.php';
 		}
 		
 		.PwAResponsiveContainerNoBG {
-				background: transparent;
-				border-style: none;  
+			background: transparent;
+			border-style: none;  
 		}
+		
+		.PwAResponsiveContainerLightGrey {
+			background: linear-gradient(#FEFEFE, #E2E2E2);
+		}
+		
+		.PwAResponsiveContainerDarkGrey {
+			background: transparent;
+		}
+		
 		
 		/* alignment of the actual image */
 		.PwAResponsive {
 			margin-left: auto;
 			margin-right:auto;
+			padding-top: 5px;
+			padding-bottom: 5px;
 			display: block !important; 
-			height: 100%;
-			width:100%;
+			height: 85%;
+			width: 90%;
 		}
 		
 		/* hide the button we create, essential: in the JS code below, the click action gets assigned to the div */
@@ -61,14 +72,14 @@ require_once 'config.php';
         /* Desktop and tablet */
         @media only screen and (min-width: 768px) {
             #walletWidgetDivRegular {width: 400px; height: 228px;}
-            .PwAResponsiveContainer, .PwAResponsiveContainerNoBG {
+            .PwAResponsiveContainer, .PwAResponsiveContainerNoBG, .PwAResponsiveContainerLightGrey, .PwAResponsiveContainerDarkGrey {
             	height:90px;
             }
         }
         
         @media only screen and (max-width: 400px) {
             #walletWidgetDivRegular {width: 400px; height: 228px;}
-            .PwAResponsiveContainer, .PwAResponsiveContainerNoBG {
+            .PwAResponsiveContainer, .PwAResponsiveContainerNoBG, .PwAResponsiveContainerLightGrey, .PwAResponsiveContainerDarkGrey {
             	height:40px;
             }
         }
@@ -80,13 +91,64 @@ require_once 'config.php';
 	<p class="warning">Please do not use this in live integrations, this is for demo purposes only.</p>
 
 	<div id="AmazonPayButton" class="PwAResponsiveContainer">
-		<img class="PwAResponsive" src="img/en_pwa.svg"></img>
+		<img class="PwAResponsive" src="img/black/pwa-logo-de.svg"></img>
+	</div>
+	
+	<div id="AmazonPayButton_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-de.svg"></img>
 	</div>
 	
 	<br />
 	<div id="AmazonPayButton2" class="PwAResponsiveContainer">
-		<img class="PwAResponsive" src="img/de_pwa.svg"></img>
+		<img class="PwAResponsive" src="img/black/pwa-logo-en.svg"></img>
 	</div>
+	
+	<div id="AmazonPayButton2_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-en.svg"></img>
+	</div>
+	
+	<br />
+	<div id="AmazonPayButton3" class="PwAResponsiveContainer">
+		<img class="PwAResponsive" src="img/black/pwa-logo-es.svg"></img>
+	</div>
+	
+	<div id="AmazonPayButton3_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-es.svg"></img>
+	</div>
+	
+	<br />
+	<div id="AmazonPayButton4" class="PwAResponsiveContainer">
+		<img class="PwAResponsive" src="img/black/pwa-logo-fr.svg"></img>
+	</div>
+	
+	<div id="AmazonPayButton4_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-fr.svg"></img>
+	</div>
+	
+	<br />
+	<div id="AmazonPayButton5" class="PwAResponsiveContainer">
+		<img class="PwAResponsive" src="img/black/pwa-logo-it.svg"></img>
+	</div>
+	
+	<div id="AmazonPayButton5_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-it.svg"></img>
+	</div>
+	
+	<br />
+	<div id="AmazonPayButton6" class="PwAResponsiveContainer">
+		<img class="PwAResponsive" src="img/black/pwa-logo-nl.svg"></img>
+	</div>
+	
+	<div id="AmazonPayButton6_s" class="PwAResponsiveContainerLightGrey">
+		<img class="PwAResponsive" src="img/styled/pwa-logo-nl.svg"></img>
+	</div>
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	<div id="walletWidgetDivRegular"></div>
@@ -135,7 +197,17 @@ require_once 'config.php';
 			
 			/* This is setting the click event to the div emulating our button */
 	        document.getElementById("AmazonPayButton").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton_s").addEventListener("click", authorization);
 	        document.getElementById("AmazonPayButton2").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton2_s").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton3").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton3_s").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton4").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton4_s").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton5").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton5_s").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton6").addEventListener("click", authorization);
+	        document.getElementById("AmazonPayButton6_s").addEventListener("click", authorization);
     		
 			OffAmazonPayments.Button("AmazonPayButton", "<?php echo $config['merchant_id']; ?>",
 			{
