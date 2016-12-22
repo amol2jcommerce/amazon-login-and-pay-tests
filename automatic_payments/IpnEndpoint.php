1ef7c1aa86020f537c93861e37eed3f8b249f9b7
Daniel Neu <danielneu81@gmail.com>
2016-12-22 15:26:29 +0000

    added IPN Logging

--- automatic_payments/IpnEndpoint.php
+++ automatic_payments/IpnEndpoint.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'IpnHandler.php';
+namespace PayWithAmazon;
+require_once 'PayWithAmazon/IpnHandler.php';
+require_once 'Logger.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +14,8 @@ $headers = array_change_key_case(getallheaders());
 $body = file_get_contents('php://input');
 try{
     $handler = new IpnHandler($headers, $body);
+    $logger = new \Psr\Log\Logger();
+    $handler->setLogger($logger);
     fwrite($f, $handler->toJson());
 } catch (Exception $e){
     fwrite($f,$e->getMessage());
